da3a77d19cc4c3d073db50ef284ab195f4010a97
Michael Guidetti <mguidetti@users.noreply.github.com>
2022-09-17 10:43:43 -0400

    V2

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## [2.0.0] - 2022-09-17
+
+- Rewrite JS.
+- Update quotes API URL.
+- Use system's dark mode setting.
+- Use JSON file for fallback quotes.
+- Update styling to match TCI main site.
+- Upgrade Chrome manifest to v3.
+- Added changelog.
--- app.js
+++ app.js
@@ -1,59 +1,37 @@
-(function () {
-  var randomizedNumber = function (length) {
-    return Math.floor(Math.random() * length);
-  }
-
-  var Quote = {
-    htmlEncode: function (str) {
-      return str.replace(/&(r|l)dquo;/g, '&$1squo;')
-                .replace(/\u201D/g, '&rsquo;')
-                .replace(/\u201C/g, '&lsquo;');
-    },
-
-    random: function (data) {
-      return data[randomizedNumber(data.length)];
-    },
-
-    injector: function (data) {
-      var quote = this.random(data);
-      var text = this.htmlEncode(quote.text);
-
-      document.getElementsByClassName('quote__text')[0].innerHTML = '&ldquo;'.concat(text, '&rdquo;');
-      document.getElementsByClassName('quote__article-title')[0].innerHTML = quote.post.title;
-      document.getElementsByClassName('quote__article-url')[0].href = quote.post.url;
-      document.getElementsByClassName('quote__article-url')[0].innerHTML = quote.post.url;
+async function fetchQuotes () {
+  try {
+    const response = await fetch('https://thecreativeindependent.com/api/v1/quotes.json')
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`)
     }
-  }
 
-  fetch('http://tci-devsite.s3-website-us-east-1.amazonaws.com/api/v1/quotes.json').then(function (response) {
-    if (response.status !== 200) {
-      Quote.injector(window.fallbackData);
-    }
-    response.json().then(function (data) {
-      Quote.injector(data);
-    });
-    document.getElementsByClassName('quote')[0].classList.add('quote--loaded');
+    return await response.json()
+  } catch (error) {
+    console.error(`Could not fetch quotes: ${error}. Using fallback quotes`)
+
+    const response = await fetch('./fallback_quotes.json')
+    return await response.json()
+  }
+}
+
+function chooseRandom (array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+function fillQuote (quote) {
+  const title = document.getElementById('quote__article-title')
+
+  document.getElementById('quote__text').innerHTML = quote.text
+  title.innerHTML = quote.post.title
+  title.href = quote.post.url
+
+  document.getElementById('quote').classList.remove('loading')
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetchQuotes().then(quotes => {
+    const quote = chooseRandom(quotes)
+    fillQuote(quote)
   })
-  .catch(function (err) {
-    Quote.injector(window.fallbackData);
-  });
-  
-  // Night Mode
-  (function () {
-    date = new Date;
-    hours = date.getHours();
-
-    if (hours <= 5 || hours >= 19) {
-      var body = document.querySelector('body');
-      var spiral = document.querySelectorAll('.cls-1');
-      var quote = document.querySelector('.quote__text');
-
-      body.classList.add('night-bg');
-      quote.classList.add('night-quote');
-
-      spiral.forEach(function(pixel) {
-        pixel.classList.add('night-spiral');
-      });
-    }
-  })();
-})();
+})
